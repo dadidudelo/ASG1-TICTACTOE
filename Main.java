e68492484c2c02c70a11a9ec169496cab03dcb7a
dadidudelo <adeliachristi123@gmail.com>
2025-02-20 20:58:16 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Welcome to Tic-Tac-Toe!");
         Game game = new Game();
         game.play();
