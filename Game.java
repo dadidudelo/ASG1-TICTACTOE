347bb86c1efda1b16891afc6ce41017f960e865c
dadidudelo <adeliachristi123@gmail.com>
2025-02-20 20:59:25 +0700

    Game.java

--- Game.java
+++ Game.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Game {
+public class Game{
     private Board board;
     private Player player1;
     private Player player2;
@@ -8,7 +8,7 @@ public class Game {
     private Scanner scanner;
     private boolean gameEnd;
 
-    public Game() {
+    public Game(){
         board = new Board();
         scanner = new Scanner(System.in);
         player1 = new Player("Player 1", 'O');
@@ -17,26 +17,26 @@ public class Game {
         gameEnd = false;
     }
 
-    public void play() {
-        while (!gameEnd) {
+    public void play(){
+        while(!gameEnd){
             board.printBoard();
             System.out.print(currentPlayer.getName() + " move (x y): ");
             int x = scanner.nextInt();
             int y = scanner.nextInt();
 
-            if (!board.placeMove(x, y, currentPlayer.getSymbol())) {
+            if(!board.placeMove(x, y, currentPlayer.getSymbol())){
                 System.out.println("Invalid move! Try again.");
                 continue;
             }
 
-            if (board.checkWin(currentPlayer.getSymbol())) {
+            if(board.checkWin(currentPlayer.getSymbol())){
                 board.printBoard();
                 System.out.println(currentPlayer.getName() + " WIN!");
                 gameEnd = true;
                 break;
             }
 
-            if (board.isTie()) {
+            if(board.isTie()){
                 board.printBoard();
                 System.out.println("It's a tie!");
                 gameEnd = true;
@@ -47,7 +47,7 @@ public class Game {
         }
     }
 
-    private void switchTurn() {
+    private void switchTurn(){
         currentPlayer = (currentPlayer == player1) ? player2 : player1;
     }
 }
